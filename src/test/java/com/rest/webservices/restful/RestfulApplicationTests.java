e1678414c5d5b518ccb7bcf68d14e38478b257ca
ugursenoll <ugursenoll96@gmail.com>
2021-02-02 22:12:56 +0300

    first commit

---
+++ src/test/java/com/rest/webservices/restful/RestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.webservices.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
