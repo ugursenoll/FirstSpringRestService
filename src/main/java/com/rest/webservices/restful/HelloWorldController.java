ee90387bac272e931b48e9e2314eb02962c62ca3
ugursenoll <ugursenoll96@gmail.com>
2021-02-04 02:06:45 +0300

    Add new Controller and class

--- src/main/java/com/rest/webservices/restful/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restful/HelloWorldController.java
@@ -1,18 +1,25 @@
 package com.rest.webservices.restful;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    //GET
-    //URI - /hello-world
-    //method - "Hello World"
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    //hello-world-bean
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format( "Hello World %s",name));
     }
 }
