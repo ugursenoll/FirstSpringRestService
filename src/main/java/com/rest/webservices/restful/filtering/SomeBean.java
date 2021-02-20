f2169ed8f8e11d9d4e26c602d2c15503287f99f0
ugursenoll <ugursenoll96@gmail.com>
2021-02-20 22:11:04 +0300

    Actuator , HAL browser and static filtering

---
+++ src/main/java/com/rest/webservices/restful/filtering/SomeBean.java
@@ -0,0 +1,24 @@
+package com.rest.webservices.restful.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(value = {"field1"})
+public class SomeBean {
+    private String field1;
+    private String field2;
+
+    @JsonIgnore
+    private String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+
+    }
+}
